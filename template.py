6336b4db4f97baf4385a06f0f5bb3ae32a61e26c
Lucky <deepankarsharma2003@gmail.com>
2023-07-04 20:54:58 +0530

    folder structure added

---
+++ template.py
@@ -0,0 +1,52 @@
+import os
+import pathlib
+from pathlib import Path
+import logging
+
+
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s: ')
+
+project_name= "TextSummarizer"
+
+list_of_files= [
+    '.github/workflows/.gitkeep',
+    f'src/{project_name}/__init__.py',
+    f'src/{project_name}/components/__init__.py',
+    f'src/{project_name}/utils/__init__.py',
+    f'src/{project_name}/utils/common.py',
+    f'src/{project_name}/logging/__init__.py',
+    f'src/{project_name}/config/__init__.py',
+    f'src/{project_name}/config/configuration.py',
+    f'src/{project_name}/pipeline/__init__.py',
+    f'src/{project_name}/entity/__init__.py',
+    f'src/{project_name}/constants/__init__.py',
+    'config/config.yaml',
+    'params.yaml',
+    'app.py',
+    'main.py',
+    'Dockerfile',
+    'setup.py',
+    'requirements.txt',
+    'research/trials.ipynb',
+    'README.md',
+    'LICENSE',
+    '.gitignore'
+]
+
+
+for filepath in list_of_files:
+    filepath= Path(filepath)
+    
+    file_dir, file_name= os.path.split(filepath)
+
+    if file_dir!='':
+        os.makedirs(file_dir, exist_ok=True)
+        logging.info(f'Created directory: {file_dir} for the file: {file_name}')
+    
+    if (not os.path.exists(filepath)) or os.path.getsize(filepath)==0:
+        with open(filepath, 'w') as f:
+            pass
+        logging.info(f'Created empty file: {filepath}')
+
+    else:
+        logging.info(f'File already exists: {filepath}')
